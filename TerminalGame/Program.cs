6c7e6ce6aa7a3876853e3745664d580a25577290
HappyElectron <ned.fpartridge@gmail.com>
2022-03-01 10:50:13 +1100

    Basic Code
    basic stuff for user classes/main classes and that

--- TerminalGame/Program.cs
+++ TerminalGame/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Matthew is stinky");
+﻿using System;
+namespace WizardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(Convert.ToString(Console.ReadLine()));
+            user.Stinky();
+        }
+    }
+    public class User
+    {
+        public string Name { get; set; }
+        public User(string name)
+        {
+            Name = name;
+        }
+        public void Stinky()
+        {
+            Console.WriteLine("{0} is stinky", Name);
+        }
+    }
+}
