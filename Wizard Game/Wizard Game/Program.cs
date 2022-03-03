d7e2ac40daa97df1fd16cd710bc4612e6f28e795
Aedin01 <95318623+Aedin01@users.noreply.github.com>
2022-03-03 12:05:34 +1100

    added some program shit

--- Wizard Game/Wizard Game/Program.cs
+++ Wizard Game/Wizard Game/Program.cs
@@ -8,6 +8,35 @@ namespace WizardGame
         {
             TutorialClass tutorial = new TutorialClass();
             tutorial.Tutorial();
+            Program program = new Program();
+            program.MainGame();
+        }
+        public void MainGame()
+        {
+            while(wizard.level == 1)
+            {
+                Monster gobli = new 
+            }
+            while(wizard.level == 2)
+            {
+                goblin
+            }
+            while(wizard.level == 3)
+            {
+                goblin
+            }
+            while(wizard.level == 4)
+            {
+                goblin
+            }
+            while(wizard.level == 5)
+            {
+                goblin
+            }
+        }
+        public void AttackSequence()
+        {
+
         }
     }
 }
