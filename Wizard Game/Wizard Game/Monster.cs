56e1c719660212986b07649afa5ee25ab6a40656
HappyElectron <90774347+HappyElectron@users.noreply.github.com>
2022-03-08 10:42:47 +1100

    updated some program.cs stuff
    maybe a change to monster too

--- Wizard Game/Wizard Game/Monster.cs
+++ Wizard Game/Wizard Game/Monster.cs
@@ -9,8 +9,7 @@ namespace WizardGame
     public class Monster
     {
         public Wizard Wizard1 { get; set; }
-        public int LowestHealth { get; set; }
-        public int HighestHealth { get; set; }
+        public int Health { get; set; }
         public int LowestXp { get; set; }
         public int HighestXp { get; set; }   
         public int LowestDamage { get; set; }
@@ -19,8 +18,7 @@ namespace WizardGame
         public string MonsterType { get; set; }
         public int damageDealt;
         public int damageTaken;
-        public int health;
-        public Monster(Wizard wizard1, int lowestXp, int highestXp, int lowestDamage, int highestDamage, string monsterName, string monsterType, int lowestHealth, int highestHealth)
+        public Monster(Wizard wizard1, int lowestXp, int highestXp, int lowestDamage, int highestDamage, string monsterName, string monsterType, int health)
         {
             Wizard1 = wizard1;
             LowestXp = lowestXp;
@@ -29,8 +27,7 @@ namespace WizardGame
             HighestDamage = highestDamage;
             MonsterName = monsterName;
             MonsterType = monsterType;
-            LowestHealth = lowestHealth;
-            HighestHealth = highestHealth;
+            Health = health;
         }
         public void Introduce()
         {
--- Wizard Game/Wizard Game/Program.cs
+++ Wizard Game/Wizard Game/Program.cs
@@ -4,6 +4,7 @@ namespace WizardGame
 {
     class Program
     {
+        public static int whichMonster;
         static void Main(string[] args)
         {
             TutorialClass tutorial = new TutorialClass();
@@ -11,30 +12,110 @@ namespace WizardGame
             Wizard wizard1 = tutorial.wizard1;
             while(wizard1.level == 1)
             {
-                Monster goblin = new Monster(wizard1,1,5,1,3,"Goblin","Earth", 2, 4);
-                while(goblin.health > 0 && wizard1.health < 0)//test function
+                Monster goblin = new Monster(wizard1,1,5,1,3,"Goblin","Earth", 3);
+                Console.WriteLine("boop");//test
+                while (goblin.Health >= 1 && wizard1.health >= 1)//test function
                 {
+                    Console.WriteLine("boop2");
                     goblin.Attack();
                     goblin.TakeDamage();
                 }
             }
             while(wizard1.level == 2)
             {
-                Monster goblin = new Monster(wizard1, 1, 5, 1, 3, "Goblin", "Earth", 2, 4);
-                Monster bigAngryBird = new Monster(wizard1, 5, 10, 3, 7, "Big Angry Bird", "Air", 4, 10);
+                Monster goblin = new Monster(wizard1, 1, 5, 1, 3, "Goblin", "Earth", 3);
+                Monster bigAngryBird = new Monster(wizard1, 5, 10, 3, 7, "Big Angry Bird", "Air", 7);
+                Random rand = new Random();
+                whichMonster = rand.Next(1, 2);
+                switch (whichMonster)
+                {
+                    case 1:
+                        while(goblin.Health > 0 && wizard1.health > 0)
+                        {
+                            goblin.Attack();
+                            goblin.TakeDamage();
+                        }
+                        break;
+                    case 2:
+                        while (bigAngryBird.Health > 0 && wizard1.health > 0)
+                        {
+                            bigAngryBird.Attack();
+                            bigAngryBird.TakeDamage();
+                        }
+                        break;
+                }
             }
             while (wizard1.level == 3)
             {
-                Monster goblin = new Monster(wizard1, 1, 5, 1, 3, "Goblin", "Earth", 2, 4);
-                Monster bigAngryBird = new Monster(wizard1, 5, 10, 3, 7, "Big Angry Bird", "Air", 4, 10);
-                Monster seaSerpent = new Monster(wizard1, 10, 20, 7, 15, "Sea Serpent", "Water", 8, 20);
+                Monster goblin = new Monster(wizard1, 1, 5, 1, 3, "Goblin", "Earth", 3);
+                Monster bigAngryBird = new Monster(wizard1, 5, 10, 3, 7, "Big Angry Bird", "Air", 7);
+                Monster seaSerpent = new Monster(wizard1, 10, 20, 7, 15, "Sea Serpent", "Water", 15);
+                Random rand = new Random();
+                whichMonster = rand.Next(1, 3);
+                switch (whichMonster)
+                {
+                    case 1:
+                        while (goblin.Health > 0 && wizard1.health > 0)
+                        {
+                            goblin.Attack();
+                            goblin.TakeDamage();
+                        }
+                        break;
+                    case 2:
+                        while (bigAngryBird.Health > 0 && wizard1.health > 0)
+                        {
+                            bigAngryBird.Attack();
+                            bigAngryBird.TakeDamage();
+                        }
+                        break;
+                    case 3:
+                        while (seaSerpent.Health > 0 && wizard1.health > 0)
+                        {
+                            seaSerpent.Attack();
+                            seaSerpent.TakeDamage();
+                        }
+                        break;
+                }
             }
             while (wizard1.level == 4)
             {
-                Monster goblin = new Monster(wizard1, 1, 5, 1, 3, "Goblin", "Earth", 2, 4);
-                Monster bigAngryBird = new Monster(wizard1, 5, 10, 3, 7, "Big Angry Bird", "Air", 4, 10);
-                Monster seaSerpent = new Monster(wizard1, 7, 10, 5, 7, "Sea Serpent", "Water", 8, 20);
-                Monster dragon = new Monster(wizard1, 20, 40, 10, 20, "Dragon", "Fire", 20, 40);
+                Monster goblin = new Monster(wizard1, 1, 5, 1, 3, "Goblin", "Earth", 3);
+                Monster bigAngryBird = new Monster(wizard1, 5, 10, 3, 7, "Big Angry Bird", "Air", 7);
+                Monster seaSerpent = new Monster(wizard1, 7, 10, 5, 7, "Sea Serpent", "Water", 15);
+                Monster dragon = new Monster(wizard1, 20, 40, 10, 20, "Dragon", "Fire", 30);
+                Random rand = new Random();
+                whichMonster = rand.Next(1, 3);
+                switch (whichMonster)
+                {
+                    case 1:
+                        while (goblin.Health > 0 && wizard1.health > 0)
+                        {
+                            goblin.Attack();
+                            goblin.TakeDamage();
+                        }
+                        break;
+                    case 2:
+                        while (bigAngryBird.Health > 0 && wizard1.health > 0)
+                        {
+                            bigAngryBird.Attack();
+                            bigAngryBird.TakeDamage();
+                        }
+                        break;
+                    case 3:
+                        while (seaSerpent.Health > 0 && wizard1.health > 0)
+                        {
+                            seaSerpent.Attack();
+                            seaSerpent.TakeDamage();
+                        }
+                        break;
+                    case 4:
+                        while (dragon.Health > 0 && wizard1.health > 0)
+                        {
+                            dragon.Attack();
+                            dragon.TakeDamage();
+                        }
+                        break;
+                }
             }
         }
     }
