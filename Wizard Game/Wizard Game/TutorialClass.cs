dc17758cb681ae110bb674c586d61045ba356ce5
LazarCactusMan <96327248+LazarCactusMan@users.noreply.github.com>
2022-03-16 14:38:13 +1100

    changed tutorial

--- Wizard Game/Wizard Game/TutorialClass.cs
+++ Wizard Game/Wizard Game/TutorialClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace WizardGame
 {
@@ -15,25 +15,40 @@ namespace WizardGame
             wizard1.name = Console.ReadLine();
             Console.WriteLine();
             wizard1.Introduce();
-            Console.WriteLine("\nIn this game there are two basic skills. Attack, Health.");
+            Console.WriteLine("\nIn this game there are two basic skills. Attack, and Health. You can attack using spells.");
             wizard1.IncreaseStats();
-            Console.WriteLine("\nAttack is the amount of damage your wizard does to an enemy.");
-            Console.WriteLine("Health is the amount of damage your wizard can take before it dies.");
-            Console.WriteLine("Currntly you have four spells. Different spells are effective against different enemies.");
-            Console.WriteLine("As you use spells, you will be able to check how effective it was after use.");
+            Console.WriteLine("For information about Attack, Health, or Spells. Press 1, 2, 3.         (Press any other key to continue)")
+           
+
+            while (true)
+            {
+                public int decision = Console.ReadLine();
+                switch (decision)
+                    {
+                    case 1;
+                        Console.WriteLine("\nAttack is the amount of damage your wizard does to an enemy.")
+                        continue;
+                    case 2; 
+                         Console.WriteLine("Health is the amount of damage your wizard can take before it dies.");
+                         continue;
+                    case 3; 
+                         Console.WriteLine("Currntly you have four spells. Different spells are effective against different enemies.");
+                         Console.WriteLine("As you use spells, you will be able to check how effective it was after use.");
+                         continue;  
+                     default; 
+                         break;
+               }
+            }
+            
             Console.WriteLine("It's time to fight your first enemy, a goblin! When you are ready, press enter.\n");
             Console.ReadLine();
             Console.WriteLine("\nYou are attacked by a goblin!");
             Console.WriteLine("Wizard Game is a turn based game. You currently have one option. Attack! Type (Attack) to select it.");
             Console.ReadLine();
-            Console.WriteLine("Yowzers! You beat the goblin!\nNow you will enter the main game. Press any key to continue.");
-            Console.ReadLine();
+            Console.WriteLine("Yowzers! You beat the goblin!\nNow you will enter the main game.");
             Console.ForegroundColor = ConsoleColor.Red;
-
-
-
-
-
+            Console.WriteLine("Press any key to continue. . .")
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
