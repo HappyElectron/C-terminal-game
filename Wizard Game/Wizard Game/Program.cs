43b1772aa550b299d6faca53e8d2239a2f6afa40
HappyElectron <90774347+HappyElectron@users.noreply.github.com>
2022-03-23 13:17:53 +1100

    this seems to fix stuff
    was an error were endgame.function() ran before everything else, for whatever reason this fixes it

--- Wizard Game/Wizard Game/Program.cs
+++ Wizard Game/Wizard Game/Program.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 
-namespace/*walter white from breaking bad*/ WizardGame
+namespace WizardGame
 {
-    class /*walter white from breaking bad*/Program
-    {/*walter white from breaking bad*/
-        public static TutorialClass /*walter white from breaking bad*/tutorial = new TutorialClass();
-        public/*walter white from breaking bad*/ static Wizard wizard1 = tutorial.wizard1;
-        static /*walter white from breaking bad*/void Main/*walter white from breaking bad*/(string[] /*walter white from breaking bad*/args)/*walter white from breaking bad*/
+    class Program
+    {
+        public static TutorialClass tutorial = new TutorialClass();
+        public static Wizard wizard1 = tutorial.wizard1;
+        static void Main(string[] args)
         {
             tutorial.Tutorial();
             while (true)
             {
-                if /*walter white from breaking bad*/(wizard1.level > 4)
+                CreateMonsters createMonsters = new CreateMonsters();
+                createMonsters.createMonsters_InitializeCombat();
+                if (wizard1.level >= 4)
                 {
-                    /*walter white from breaking bad*/
-                    EndGame /*walter white from breaking bad*/endgame = new/*walter white from breaking bad*/ EndGame();
-                    endgame/*walter white from breaking bad*/.function();
+                    EndGame endgame = new EndGame();
+                    endgame.function();
                 }
-                CreateMonsters /*walter white from breaking bad*/createMonsters = new CreateMonsters();
-                createMonsters/*walter white from breaking bad*/.createMonsters_InitializeCombat()/*walter white from breaking bad*/;
             }
         }
     }
